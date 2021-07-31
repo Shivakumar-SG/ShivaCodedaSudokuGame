2ef84d1b16de33d06d93708f46c92459bb1395d1
Shivakumar <shivakumarsgangamath@gmail.com>
2021-07-31 17:22:18 +0530

    Random Sudoku Generated with Solution

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-//load boards
-const easy = [
-    "6------7------5-2------1---362----81--96-----71--9-4-5-2---651---78----345-------",
-    "685329174971485326234761859362574981549618732718293465823946517197852643456137298"
-  ];
-  const medium = [
-    "--9-------4----6-758-31----15--4-36-------4-8----9-------75----3-------1--2--3--",
-    "619472583243985617587316924158247369926531478734698152891754236365829741472163895"
-  ];
-  const hard = [
-    "-1-5-------97-42----5----7-5---3---7-6--2-41---8--5---1-4------2-3-----9-7----8--",
-    "712583694639714258845269173521436987367928415498175326184697532253841769976352841"
-  ];
 
 //creating vars
 var timer;
@@ -20,7 +7,7 @@ var selectedNum;
 var selectedTile;
 var disableSelect;
 
-//function to element id
+//function to get html element ids
 function id(id) {
     return document.getElementById(id);
 }
@@ -35,6 +22,8 @@ function qsa(selector) {
 
 //create new game button click handler
 window.onload = function() {
+    //show start game button
+    id("startBtn").classList.remove("hidden");
     id('startBtn').addEventListener("click", startGame);
 
     //add event listener to each number in numberController
@@ -64,18 +53,31 @@ window.onload = function() {
 }
 
 function startGame() {
-  //Choose board difficulty
+
+  //hide start game button when game starts
+  id("startBtn").classList.add("hidden");
+
+  //initiate the sudoku generator
+  initiate();
+  //difficulty method answers. test is partial sudoku keys. myArr is complete tile keys
+  window.easy = [test,myArr];
+  window.medium = [test, myArr];
+  window.hard = [test, myArr];
+
+  //Choose board difficulty and set lives
   let board;
   if (id("diff1").checked) {
     board = easy[0];
+    lives = 20;
   }else if(id("diff2").checked) {
     board = medium[0];
+    lives = 15;
   }else {
+    lives = 10;
     board = hard[0];
   }
 
   //set lives
-  lives = 5;
   disableSelect = false;
   id("lives").textContent = 'Lives Remaining: ' + lives;
   //crate board based on difficulty
@@ -93,14 +95,37 @@ function startGame() {
 
   //show numberContainer
   id("numberContainer").classList.remove("hidden");
+  // show solve button when game started.
+  id("solveButton").classList.remove("hidden");
+  // add function on click.
+  id("solveButton").addEventListener("click", solveCurrentSudoku);
 }
+
+//solving the current sudoku. This function gets called after clicking solve
+function solveCurrentSudoku(){
+  for (let i = 0; i < 81; i++) {
+
+    // pick out tiles and fill the solution keys
+     let ansTile = id(i);
+     ansTile.textContent = myArr[i];
+  }
+  id("lives").textContent = "Try Harder Next Time Buddy 😊";
+  //disable moves and stop the timer
+  disableSelect = true;
+  clearTimeout(timer);
+
+  // Show start game button again and hide solve button when game finished
+  id("startBtn").classList.remove("hidden");
+  id("solveButton").classList.add("hidden");
+}
+
 
 //timer function
 function startTimer() {
   //set time based on input
-  if ( id("timer1").checked ) timeRemaining = 180;
-  else if ( id("timer2").checked) timeRemaining = 300;
-  else timeRemaining = 600;
+  if ( id("timer1").checked ) timeRemaining = 300;
+  else if ( id("timer2").checked) timeRemaining = 600;
+  else timeRemaining = 900;
 
   //set time for first time
   id("timer").textContent = timeConversion(timeRemaining);
@@ -134,9 +159,9 @@ function generateBoard(board) {
     //create new paragraph element
     let tile = document.createElement("p");
     //if the tile is not supposed to be blank,
-    if (board.charAt(index) != "-") {
+    if (board[index] != 0) {
       //set tile text to correctnumber
-      tile.textContent = board.charAt(index);
+      tile.textContent = board[index];
     } else {
       //add click event listener to tile element
       tile.addEventListener("click", function(){
@@ -200,6 +225,9 @@ function updateMove() {
       //check if the board is completed
       if (checkDone()) {
         endGame();
+        //  when the game ends, show start button and hide solveButton
+        id("startBtn").classList.remove("hidden");
+        id("solveButton").classList.add("hidden");
       }
 
       //if the number does not match the number in the solution
@@ -236,10 +264,30 @@ function updateMove() {
     }
   }
 }
+function checkCorrect(tile) {
+  // set solution based on the difficulty
+  let solution;
+  if (id("diff1").checked) {
+    solution = easy[1];
+  }else if(id("diff2").checked) {
+    solution = medium[1];
+  }else {
+    solution = hard[1];
+  }
+
+  // if tile number is equal to the solution number
+  if (solution[(tile.id)] == tile.textContent ) {
+    return true;
+  }else {
+    return false;
+  }
+}
 
 //checkDone func
 function checkDone() {
   let tiles = qsa(".tile");
+
+  // if any tile is empty then continue game
   for (let i = 0; i < tiles.length; i++) {
     if (tiles[i].textContent === "") {
       return false;
@@ -262,24 +310,6 @@ function endGame() {
   }
 }
 
-function checkCorrect(tile) {
-  // set solution based on the difficulty
-  let solution;
-  if (id("diff1").checked) {
-    solution = easy[1];
-  }else if(id("diff2").checked) {
-    solution = medium[1];
-  }else {
-    solution = hard[1];
-  }
-
-  // if tile number is equal to the solution number
-  if (solution.charAt(tile.id) === tile.textContent ) {
-    return true;
-  }else {
-    return false;
-  }
-}
 
 //clears privious board
 function clearPrevious() {
@@ -303,4 +333,246 @@ function clearPrevious() {
   //clear selected variables
   selectedTile = null;
   selectedNum = null;
+}
+
+
+// random Board numbers and correct sudoku numbers are generated
+function initiate() {
+  // null -> null
+  // populate the board with initial random values using randBoard()
+  randBoard();
+  var startingBoard = [[]]
+  var j = 0;
+
+  // test-array with vales generated
+  var sud = test;
+  // push elements to board
+  for (var i = 1; i <= 81; i++){
+      const val = sud[i-1];
+      if (val == 0){
+        startingBoard[j].push(null)
+      }
+      else { 
+          startingBoard[j].push(Number(val))
+      }
+      if (i % 9 == 0 && i < 81){
+          startingBoard.push([])
+          j++
+      }
+  }
+  
+  // check if the board is valid.
+  const inputValid = validBoard(startingBoard)
+  if (!inputValid){
+      // if input Is Invalid call initiate() again to get valid board numbers
+      initiate();
+  }
+  else{
+    // if answer is valid, fill the board numbers to myArr
+      const answer = solve(startingBoard)
+      window.myArr = new Array(81);
+      var x = 0;
+      if (answer == undefined || answer == 0) {
+        initiate();
+      } else {
+        for (let i = 0; i < 9; i++) {
+          for (let j = 0; j < 9; j++) {
+            myArr[x] = answer[i][j];
+            x++;
+          }
+        }
+      }
+  }
+}
+
+// function to generate random numbers[1-9] in random places[1-81]
+function randBoard() {
+  // generate size of random places to which numbers are to be generated
+  if (id("diff1").checked) {
+    var arrSize = Math.floor(Math.random() * (27-23+1) + 23);
+  }else if(id("diff2").checked) {
+    var arrSize = Math.floor(Math.random() * (27-25+1) + 25);
+  }else {
+    var arrSize = Math.floor(Math.random() * (25-20+1) + 20);
+  }
+  var arr = Array(arrSize);
+
+  // generate random numbers between 0 and 80
+  for(var i = 0; i < arr.length; i++){
+      var item = Math.floor(Math.random() * (80-0+1) + 0);
+      arr[i] = item;
+  }
+  // global array test with default 0 values
+  window.test = new Array(81).fill(0);
+
+  // generate sudoku numbers [1-9] in the places generated above
+  for (let j = 0; j < test.length; j++) {
+    test[arr[j]] = Math.floor(Math.random() * (9-0+1) + 0);
+  }
+  return test;
+}
+
+// solves the given sudoku board
+  // ASSUME the given sudoku board is valid
+function solve(board) {
+  
+  // if board is already solved, return board
+  if (solved(board)) {
+      return board
+  }
+  else {
+      // check for possible boards
+      const possibilities = nextBoards(board)
+      // keep only valid boards
+      const validBoards = keepOnlyValid(possibilities)
+
+      // search for the solution for the board
+      return searchForSolution(validBoards)
+  }
+}
+
+// finds a valid solution to the sudoku problem
+function searchForSolution(boards){
+  if (boards.length < 1){
+      return false
+  }
+  else {
+      // backtracking search for solution
+      var first = boards.shift()
+      const tryPath = solve(first)
+      if (tryPath != false){
+          return tryPath
+      }
+      else{
+          return searchForSolution(boards)
+      }
+  }
+}
+
+
+function solved(board){
+  // Board -> Boolean
+  // checks to see if the given puzzle is solved
+  for (var i = 0; i < 9; i++){
+      for (var j = 0; j < 9; j++){
+          if (board[i][j] == null){
+              return false
+          }
+      }
+  }
+  return true
+}
+
+
+function nextBoards(board){ 
+  // Board -> List[Board]
+  // finds the first emply square and generates 9 different boards filling in that square with numbers 1...9
+  var res = []
+  const firstEmpty = findEmptySquare(board)
+  if (firstEmpty != undefined){
+      const y = firstEmpty[0]
+      const x = firstEmpty[1]
+      for (var i = 1; i <= 9; i++){
+          var newBoard = [...board]
+          var row = [...newBoard[y]]
+          row[x] = i
+          newBoard[y] = row
+          res.push(newBoard)
+      }
+  }
+  return res
+}
+
+function findEmptySquare(board){
+  // Board -> [Int, Int] 
+  // (get the i j coordinates for the first empty square)
+  for (var i = 0; i < 9; i++){
+      for (var j = 0; j < 9; j++){
+          if (board[i][j] == null) {
+              return [i, j]
+          }
+      }
+  }
+}
+
+
+function keepOnlyValid(boards){
+  // List[Board] -> List[Board]
+  // filters out all of the invalid boards from the list
+  var res = []
+  for (var i = 0; i < boards.length; i++){
+      if (validBoard(boards[i])){
+          res.push(boards[i])
+      }
+  }
+  return res
+}
+
+
+function validBoard(board){
+  // Board -> Boolean
+  // checks to see if given board is valid
+  return rowsGood(board) && columnsGood(board) && boxesGood(board)
+}
+
+function rowsGood(board){
+  // Board -> Boolean
+  // makes sure there are no repeating numbers for each row
+  for (var i = 0; i < 9; i++){
+      var cur = []
+      for (var j = 0; j < 9; j++){
+          if (cur.includes(board[i][j])){
+              return false
+          }
+          else if (board[i][j] != null){
+              cur.push(board[i][j])
+          }
+      }
+  }
+  return true
+}
+
+function columnsGood(board){
+  // Board -> Boolean
+  // makes sure there are no repeating numbers for each column
+  for (var i = 0; i < 9; i++){
+      var cur = []
+      for (var j = 0; j < 9; j++){
+          if (cur.includes(board[j][i])){
+              return false
+          }
+          else if (board[j][i] != null){
+              cur.push(board[j][i])
+          }
+      }
+  }
+  return true
+}
+
+
+function boxesGood(board){
+  // transform this everywhere to update res
+  const boxCoordinates = [[0, 0], [0, 1], [0, 2],
+                          [1, 0], [1, 1], [1, 2],
+                          [2, 0], [2, 1], [2, 2]]
+  // Board -> Boolean
+  // makes sure there are no repeating numbers for each box
+  for (var y = 0; y < 9; y += 3){
+      for (var x = 0; x < 9; x += 3){
+          // each traversal should examine each box
+          var cur = []
+          for (var i = 0; i < 9; i++){
+              var coordinates = [...boxCoordinates[i]]
+              coordinates[0] += y
+              coordinates[1] += x
+              if (cur.includes(board[coordinates[0]][coordinates[1]])){
+                  return false
+              }
+              else if (board[coordinates[0]][coordinates[1]] != null){
+                  cur.push(board[coordinates[0]][coordinates[1]])
+              }
+          }
+      }
+  }
+  return true
 }
